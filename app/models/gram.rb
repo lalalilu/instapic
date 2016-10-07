7449cecc95afd3740ce1a68b2281beef2915f247
Lulu Li <lilulu004@gmail.com>
2016-10-07 20:10:46 +0000

    Connect post to user

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,3 +1,5 @@
 class Gram < ActiveRecord::Base
   validates :message, presence: true
+  
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007151907) do
+ActiveRecord::Schema.define(version: 20161007182831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20161007151907) do
     t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "grams", ["user_id"], name: "index_grams_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
